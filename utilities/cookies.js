5335b90c4594fb039ee31e4dee0e6a9fff38357f
amar99587 <72984889+amar99587@users.noreply.github.com>
2023-10-26 07:49:26 +0100

    Update cookies.js

--- utilities/cookies.js
+++ utilities/cookies.js
@@ -2,10 +2,14 @@ const { sign, verify } = require("jsonwebtoken");
 
 const jwtSecret = process.env.jwt_secret_key;
 
+exports.options = ({ maxAge }) => {
+    return { defaultValue: { domain: process.env.cors_origin, SameSite: "Strict", secure: true, httpOnly: true, maxAge: maxAge * 1000 } };
+}; 
+
 exports.create = (data, CustomMaxAge) => {
     const maxAge = CustomMaxAge || (30 * 24 * 60 * 60); //  default value : 30 days in seconds
     const token = sign(data, jwtSecret, { expiresIn: maxAge });
-    return { token, defaultValue: { httpOnly: true, maxAge: maxAge * 1000 } };
+    return { token, ...options({ maxAge }) };
 };
 
 exports.read = (token = false) => {
