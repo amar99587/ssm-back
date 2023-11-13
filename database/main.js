25c79c10cca8ec18aa8be31995f138427a7ceda4
amar99587 <72984889+amar99587@users.noreply.github.com>
2023-11-13 21:18:53 +0100

    update

--- database/main.js
+++ database/main.js
@@ -1,10 +1,10 @@
 const { Pool } = require("pg");
 
 const client = new Pool({
-        connectionString: process.env.db_host,
-        ssl: {
-            rejectUnauthorized: false
-        }
-    });
+    connectionString: process.env.db_host,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 module.exports = client;
